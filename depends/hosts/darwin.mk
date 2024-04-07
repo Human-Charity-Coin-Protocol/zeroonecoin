b7d3fbbe850b192763649bfc6ea36f46cefd4e55
KingricharDV#0439 <kingrichardv@protonmail.com>
2024-04-07 16:31:36 -0400

    update mac os

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,7 +1,9 @@
-OSX_MIN_VERSION=10.8
+OSX_MIN_VERSION=10.15
 OSX_SDK_VERSION=11.3
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
-LD64_VERSION=253.9
+XCODE_VERSION=12.2
+XCODE_BUILD_ID=12B45b
+LD64_VERSION=609
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -18,4 +18,9 @@ darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_native_packages+= native_clang
+endif
+
 endif
