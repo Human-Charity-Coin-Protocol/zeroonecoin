f075d10a0117a7c8f40bbde5d3e967f9ba15b042
KingricharDV#0439 <kingrichardv@protonmail.com>
2024-04-09 10:51:47 -0400

    Revert "update mac os"
    This reverts commit b7d3fbbe850b192763649bfc6ea36f46cefd4e55.

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,9 +1,7 @@
-OSX_MIN_VERSION=10.15
+OSX_MIN_VERSION=10.8
 OSX_SDK_VERSION=11.3
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
-XCODE_VERSION=12.2
-XCODE_BUILD_ID=12B45b
-LD64_VERSION=609
+LD64_VERSION=253.9
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
